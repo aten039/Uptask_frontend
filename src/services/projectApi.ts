3a6b2e5497530c2f26d60c8788ac38931e6bdbdf
aten039 <l.elvis039@gmail.com>
2024-09-14 13:22:39 -0400

    se agrego services para crear proyectos utilizando axios y react query

--- src/services/projectApi.ts
+++ src/services/projectApi.ts
@@ -1,13 +1,34 @@
 
-import { ProjectFormData } from "../types";
+import { dashboardProjectSchema, ProjectFormData } from "../types";
 import api from "../lib/axios";
 import { isAxiosError } from "axios";
+
 
 export async function createProject(dataForm: ProjectFormData) {
     try {
         const {data}  = await api.post('/projects', dataForm)
         
         return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response){
+            throw new Error(error.response?.data.errors.msg);
+        }
+        throw new Error('ha ocurrido un error');
+    }
+}
+
+export async function getProjects() {
+    try {
+        
+        const {data} = await api.get('/projects');
+        
+        const response = dashboardProjectSchema.safeParse(data)
+       
+        if(response.success){
+            return response.data
+        }
+        throw new Error('ha ocurrido un error');
+        
     } catch (error) {
         if(isAxiosError(error) && error.response){
             throw new Error(error.response?.data.errors.msg);
